48e412dc837845837e8669ab8faa18589eb430a7
onowak1 <oskar.nowak3.stud@pw.edu.pl>
2025-01-12 00:00:08 +0100

    visual changes with orientation

--- app/src/main/java/com/example/timetracker/ui/tasks/edit_task/EditTaskDialogFragment.java
+++ app/src/main/java/com/example/timetracker/ui/tasks/edit_task/EditTaskDialogFragment.java
@@ -52,7 +52,7 @@ public class EditTaskDialogFragment extends DialogFragment {
         if (dialog != null)
         {
             int width = ViewGroup.LayoutParams.MATCH_PARENT;
-            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.WRAP_CONTENT;
             dialog.getWindow().setLayout(width, height);
         }
     }
